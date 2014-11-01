430541141cea0009960597fc93b0250e77eadba4
ryliao0221 <ryliao0221@gmail.com>
2014-11-01 04:12:22 -0700

    finished Feed view's backbone

---
+++ app/src/main/java/edu/ucsd/stuffly/FeedArrayAdapter.java
@@ -0,0 +1,74 @@
+package edu.ucsd.stuffly;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+/**
+ * Created by ryanliao on 11/1/14.
+ */
+public class FeedArrayAdapter extends ArrayAdapter<String>
+{
+    private final Activity context;
+
+    public String[] feed_textview1_text;
+    public String[] feed_textview2_text;
+    public Image[] feed_imageview_image;
+
+
+    public FeedArrayAdapter(Activity context, String[] text1, String[] text2 )//, Image[] image)
+    {
+        super(context, R.layout.cell_feed, text1);
+        this.context = context;
+        this.feed_textview1_text = text1;
+        this.feed_textview2_text = text2;
+        //this.feed_imageview_image = image;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.cell_feed, parent, false);
+
+        if(position % 2 == 1)
+        {
+            LinearLayout bg = (LinearLayout) rowView.findViewById(R.id.cell_feed_linearlayout);
+            bg.setBackgroundColor(Color.parseColor("#e7e7e8")); // move this to @color later
+
+        }
+
+        String text1 = "text1 placeholder";
+        String text2 = "text2 placeholder";
+        //int imageId = getContext().getResources().getDrawable(R.id.app_logo);
+
+        TextView feed_textview1 = (TextView) rowView.findViewById(R.id.feed_textview1);
+        feed_textview1.setText(text1);
+
+        TextView feed_textview2 = (TextView) rowView.findViewById(R.id.feed_textview2);
+        feed_textview2.setText(text2);
+
+        ImageView feed_imageview = (ImageView) rowView.findViewById(R.id.feed_imageview);
+        feed_imageview.setImageResource(R.drawable.app_logo);
+
+
+
+        return rowView;
+    }
+}
+
--- app/src/main/java/edu/ucsd/stuffly/FeedFragment.java
+++ app/src/main/java/edu/ucsd/stuffly/FeedFragment.java
@@ -5,6 +5,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 import edu.ucsd.stuffly.R;
 
@@ -13,14 +17,57 @@ import edu.ucsd.stuffly.R;
  */
 public class FeedFragment extends Fragment
 {
+    ArrayList<String> feed_list;
+    ListView listview_feed;
+    FeedArrayAdapter arrayAdapter;
+
+    private String[] text1;
+    private String[] text2;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState)
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
 
         View rootView = inflater.inflate(R.layout.fragment_feed, container, false);
+        text1 = new String[10];
+        text2 = new String[10];
+
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        super.onActivityCreated(savedInstanceState);
+
+        for(int i = 0; i < 10; i++)
+        {
+            text1[i] = "text1: " + Integer.toString(i);
+            text2[i] = "text2: " + Integer.toString(i);
+        }
+
+
+        feed_list = new ArrayList<String>();
+        listview_feed = (ListView) getActivity().findViewById(R.id.listview_feed);
+        arrayAdapter = new FeedArrayAdapter(getActivity(), text1, text2);
+        listview_feed.setAdapter(arrayAdapter);
+
+
+
+//        // allow action when clicking on listview's cell
+//        listview_feed.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            ////
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                feed_list.remove(position);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//
+//        });
     }
 
 
