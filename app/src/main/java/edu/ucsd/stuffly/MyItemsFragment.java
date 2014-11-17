835efd31e2960a857b6e8bc30ed0a7ca8d1c9a38
Tansen Zhu <tansen.zhu@gmail.com>
2014-11-16 20:59:29 -0800

    spinner for myitems

--- app/src/main/java/edu/ucsd/stuffly/MyItemsFragment.java
+++ app/src/main/java/edu/ucsd/stuffly/MyItemsFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import edu.ucsd.stuffly.R;
 
@@ -19,6 +21,11 @@ public class MyItemsFragment extends Fragment
 
         View rootView = inflater.inflate(R.layout.fragment_myitems, container, false);
 
+        Spinner spinner = (Spinner) rootView.findViewById(R.id.myItemSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity().getBaseContext(),
+                R.array.my_items_array, android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return rootView;
     }
 
