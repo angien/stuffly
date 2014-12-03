b4424a04ff91e53164a9abbf3e47da8e8399be3e
Angie <angienguyen629@gmail.com>
2014-12-02 18:35:10 -0800

    try this

--- app/src/main/java/edu/ucsd/stuffly/RetrieveFeedTask.java
+++ app/src/main/java/edu/ucsd/stuffly/RetrieveFeedTask.java
@@ -21,7 +21,7 @@ import java.net.URL;
  * Created by MadnessX on 12/2/2014.
  */
 public class RetrieveFeedTask extends AsyncTask<URL, Integer, String> {
-    protected String doInBackground(URL ... urls) {
+    protected String doInBackground(String ... params) {
 //        int count = urls.length;
 //        long totalSize = 0;
 //        for (int i = 0; i < count; i++) {
@@ -31,30 +31,56 @@ public class RetrieveFeedTask extends AsyncTask<URL, Integer, String> {
 //            if (isCancelled()) break;
 //        }
 //        return totalSize;
-        StringBuilder builder = new StringBuilder();
-        HttpClient client = new DefaultHttpClient();
-        HttpGet httpGet = new HttpGet(urls[0].toString());
-        try{
-            HttpResponse response = client.execute(httpGet);
-            StatusLine statusLine = response.getStatusLine();
-            int statusCode = statusLine.getStatusCode();
-            if(statusCode == 200){
-                HttpEntity entity = response.getEntity();
-                InputStream content = entity.getContent();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(content));
-                String line;
-                while((line = reader.readLine()) != null){
-                    builder.append(line);
+
+        if (params[1].equals("GET")) {
+            StringBuilder builder = new StringBuilder();
+            HttpClient client = new DefaultHttpClient();
+            HttpGet httpGet = new HttpGet(params[0]);
+            try{
+                HttpResponse response = client.execute(httpGet);
+                StatusLine statusLine = response.getStatusLine();
+                int statusCode = statusLine.getStatusCode();
+                if(statusCode == 200){
+                    HttpEntity entity = response.getEntity();
+                    InputStream content = entity.getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(content));
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        builder.append(line);
+                    }
+                } else {
+                    Log.e(MainActivity.class.toString(), "Failedet JSON object");
                 }
-            } else {
-                Log.e(MainActivity.class.toString(), "Failedet JSON object");
+            }catch(ClientProtocolException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
             }
-        }catch(ClientProtocolException e){
-            e.printStackTrace();
-        } catch (IOException e){
-            e.printStackTrace();
+            return builder.toString();
         }
-        return builder.toString();
+        else {
+         // Create a new HttpClient and Post Header
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(params[0]);
+
+            try {
+            // Add your data
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
+                nameValuePairs.add(new BasicNameValuePair("_id", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("firstname", "AndDev is Cool!"));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+
+            // Execute HTTP Post Request
+                HttpResponse response = httpclient.execute(httppost);
+                
+            } catch (ClientProtocolException e) {
+            // TODO Auto-generated catch block
+            } catch (IOException e) {
+            // TODO Auto-generated catch block
+            }
+        }
+
+        
 
     }
 
