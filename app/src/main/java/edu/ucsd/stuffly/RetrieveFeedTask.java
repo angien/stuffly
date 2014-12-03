51d36bb47205780ba19e160c1a4d21b87f9add22
Tansen Zhu <tansen.zhu@gmail.com>
2014-12-02 18:49:00 -0800

    broken http

--- app/src/main/java/edu/ucsd/stuffly/RetrieveFeedTask.java
+++ app/src/main/java/edu/ucsd/stuffly/RetrieveFeedTask.java
@@ -5,22 +5,28 @@ import android.util.Log;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
 import org.apache.http.StatusLine;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by MadnessX on 12/2/2014.
  */
-public class RetrieveFeedTask extends AsyncTask<URL, Integer, String> {
+public class RetrieveFeedTask extends AsyncTask<String, Integer, String> {
     protected String doInBackground(String ... params) {
 //        int count = urls.length;
 //        long totalSize = 0;
@@ -68,16 +74,21 @@ public class RetrieveFeedTask extends AsyncTask<URL, Integer, String> {
                 List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2);
                 nameValuePairs.add(new BasicNameValuePair("_id", "12345"));
                 nameValuePairs.add(new BasicNameValuePair("firstname", "AndDev is Cool!"));
+                nameValuePairs.add(new BasicNameValuePair("lastname", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("email", "AndDev is Cool!"));
+                nameValuePairs.add(new BasicNameValuePair("password", "12345"));
+                nameValuePairs.add(new BasicNameValuePair("imageURL", "AndDev is Cool!"));
                 httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
 
             // Execute HTTP Post Request
                 HttpResponse response = httpclient.execute(httppost);
-                
+                return "true";
             } catch (ClientProtocolException e) {
             // TODO Auto-generated catch block
             } catch (IOException e) {
             // TODO Auto-generated catch block
             }
+            return "false";
         }
 
         
