3d4d3ca5c4e1f2e995e3f6c651f2414449bc7e8f
Angie <angienguyen629@gmail.com>
2014-12-07 17:02:35 -0800

    message template

---
+++ app/src/main/java/edu/ucsd/stuffly/MessagesArrayAdapter.java
@@ -0,0 +1,115 @@
+package edu.ucsd.stuffly;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
+import android.media.Image;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+
+/**
+ * Created by Angie on 12/7/14.
+ */
+public class MessagesArrayAdapter extends ArrayAdapter<String> {
+    private final Activity context;
+
+    public String[] messages_textview1_text;
+    public String[] messages_textview2_text;
+    public JSONObject[] messages_json;
+    public Image[] messages_imageview_image;
+
+
+    public MessagesArrayAdapter(Activity context, String[] text1, String[] text2, JSONObject[] json )//, Image[] image)
+    {
+        super(context, R.layout.cell_messages, text1);
+        this.context = context;
+        this.messages_textview1_text = text1;
+        this.messages_textview2_text = text2;
+        this.messages_json =json;
+        //this.messages_imageview_image = image;
+    }
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent)
+    {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.cell_messages, parent, false);
+
+        rowView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Context context = getContext();
+                View root = ((Activity)context).getWindow().getDecorView().findViewById(android.R.id.content);
+                String text;
+                String title = "";
+                String description = "";
+                int price = 0;
+                boolean obo = false;
+                String location = "";
+
+                try {
+                    Log.e("GET VIEW ID USER", UserID.id);
+                    text = messages_json[position].toString();
+                    title = messages_json[position].getString("title");
+                    description = messages_json[position].getString("description");
+                    price = messages_json[position].getInt("price");
+                    obo = messages_json[position].getBoolean("obo");
+                    location = messages_json[position].getString("location");
+
+                    ItemDetailFragment idf = new ItemDetailFragment();
+
+                    idf.setContent(title,description, price,obo,location);
+
+                    if ((UserID.id).equals(messages_json[position].getString("user"))) {
+                        text = "MATCHES!!!";
+                        idf.setSelf(true);
+                    }
+                    else {
+                        idf.setSelf(false);
+                    }
+                    idf.show(((FragmentActivity) context).getSupportFragmentManager(), "itemDetail" + position);
+                }
+                catch (Exception e) {
+                    text = e.toString();
+                }
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, text, duration);
+                toast.show();
+            }
+        });
+
+        //int imageId = getContext().getResources().getDrawable(R.id.app_logo);
+
+        TextView messages_textview1 = (TextView) rowView.findViewById(R.id.messages_cell_title);
+        messages_textview1.setText(this.messages_textview1_text[position]);
+
+        TextView messages_textview2 = (TextView) rowView.findViewById(R.id.messages_cell_description);
+        messages_textview2.setText(this.messages_textview2_text[position]);
+
+        //ImageView messages_imageview = (ImageView) rowView.findViewById(R.id.messages_imageview);
+        //messages_imageview.setImageResource(R.drawable.app_logo);
+
+
+
+        return rowView;
+    }
+}
--- app/src/main/java/edu/ucsd/stuffly/MessagesFragment.java
+++ app/src/main/java/edu/ucsd/stuffly/MessagesFragment.java
@@ -2,24 +2,92 @@ package edu.ucsd.stuffly;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 import edu.ucsd.stuffly.R;
 
 /**
- * Created by ryanliao on 10/31/14.
+ * Created by angien
  */
 public class MessagesFragment extends Fragment
 {
+    ArrayList<String> messages_list;
+    ListView listview_messages;
+    MessagesArrayAdapter arrayAdapter;
+
+    private String[] text1;
+    private String[] text2;
+    private JSONObject[] json;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
 
         View rootView = inflater.inflate(R.layout.fragment_messages, container, false);
+        text1 = new String[10];
+        text2 = new String[10];
+        json = new JSONObject[10];
+
 
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState)
+    {
+        MyHttpRequests rtaskget = new MyHttpRequests();
+        try{
+            rtaskget.execute("/api/post/","GET");
+            JSONArray feed = new JSONArray(rtaskget.get());
+
+            for(int i = 0; i < feed.length(); i++)
+            {
+                text1[i] = feed.getJSONObject(i).getString("title");
+                text2[i] = feed.getJSONObject(i).getString("description");
+                json[i] = feed.getJSONObject(i);
+
+                Log.i("STUFFFFFF url", text1[i]);
+                //text1[i] = "text1: " + Integer.toString(i);
+                //text2[i] = "text2: " + Integer.toString(i);
+            }
+
+
+        }catch(Exception e){
+            Log.e("FeedFragment GET", "treat yo self");
+        }
+
+
+        messages_list = new ArrayList<String>();
+        listview_messages = (ListView) getActivity().findViewById(R.id.listview_messages);
+        arrayAdapter = new MessagesArrayAdapter(getActivity(), text1, text2, json);
+        listview_messages.setAdapter(arrayAdapter);
+
+
+        super.onActivityCreated(savedInstanceState);
+
+//        // allow action when clicking on listview's cell
+//        listview_feed.setOnItemClickListener(new AdapterView.OnItemClickListener()
+//        {
+//            ////
+//            @Override
+//            public void onItemClick(AdapterView<?> parent, View view,
+//                                    int position, long id) {
+//                feed_list.remove(position);
+//                arrayAdapter.notifyDataSetChanged();
+//            }
+//
+//
+//        });
+    }
+
 
 }
